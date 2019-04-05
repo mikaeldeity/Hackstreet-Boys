a9bbee794b057a88ce49ee02db7cc0ad468cc101
Mikael Santrolli <mikael.santrolli@gmail.com>
2019-04-05 14:01:26 +0100

    updates

--- FaceMorph/MeshMorph.py
+++ FaceMorph/MeshMorph.py
@@ -3,32 +3,33 @@ import numpy as np
 
 def main():
     
+    sharepath = "C:\\Users\\Mikael\\Documents\\GitHub\\Hackstreet-Boys\\FaceMorph\\"
     path = "C:\\Users\\Mikael\\Documents\\GitHub\\Hackstreet-Boys\\"
     landmarks = read_pts(path + "FaceAlign\\facepts.pts")
     image_width = 1280 # Make sure to adjust these when using your own images!
     image_height = 720
 
-    model = eos.morphablemodel.load_model("../share/sfm_shape_3448.bin")
-    blendshapes = eos.morphablemodel.load_blendshapes("../share/expression_blendshapes_3448.bin")
+    model = eos.morphablemodel.load_model(sharepath + "share\\sfm_shape_3448.bin")
+    blendshapes = eos.morphablemodel.load_blendshapes(sharepath + "share\\expression_blendshapes_3448.bin")
     # Create a MorphableModel with expressions from the loaded neutral model and blendshapes:
     morphablemodel_with_expressions = eos.morphablemodel.MorphableModel(model.get_shape_model(), blendshapes,
                                                                         color_model=eos.morphablemodel.PcaModel(),
                                                                         vertex_definitions=None,
                                                                         texture_coordinates=model.get_texture_coordinates())
-    landmark_mapper = eos.core.LandmarkMapper('../share/ibug_to_sfm.txt')
-    edge_topology = eos.morphablemodel.load_edge_topology('../share/sfm_3448_edge_topology.json')
-    contour_landmarks = eos.fitting.ContourLandmarks.load('../share/ibug_to_sfm.txt')
-    model_contour = eos.fitting.ModelContour.load('../share/sfm_model_contours.json')
+    landmark_mapper = eos.core.LandmarkMapper(sharepath + "share\\ibug_to_sfm.txt")
+    edge_topology = eos.morphablemodel.load_edge_topology(sharepath + "share\\sfm_3448_edge_topology.json")
+    contour_landmarks = eos.fitting.ContourLandmarks.load(sharepath + "share\\ibug_to_sfm.txt")
+    model_contour = eos.fitting.ModelContour.load(sharepath + "share\\sfm_model_contours.json")
 
     (mesh, pose, shape_coeffs, blendshape_coeffs) = eos.fitting.fit_shape_and_pose(morphablemodel_with_expressions,
         landmarks, landmark_mapper, image_width, image_height, edge_topology, contour_landmarks, model_contour)
 
     # Now you can use your favourite plotting/rendering library to display the fitted mesh, using the rendering
-    # parameters in the 'pose' variable.
+    # parameters in the "pose" variable.
 
     # Or for example extract the texture map, like this:
     # import cv2
-    # image = cv2.imread('../bin/data/image_0010.png')
+    # image = cv2.imread("../bin/data/image_0010.png")
     # isomap = eos.render.extract_texture(mesh, pose, image)
     
     p = path + "FaceMeshOutput\\FaceMesh.obj"
