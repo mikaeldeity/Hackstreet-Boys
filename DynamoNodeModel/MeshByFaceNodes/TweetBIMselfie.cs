023f3569cd1cf169dfb54318610ba61ad7f45c6b
AhmmTools <44774230+AhmmTools@users.noreply.github.com>
2019-04-06 13:51:26 +0100

    updates

---
+++ DynamoNodeModel/MeshByFaceNodes/TweetBIMselfie.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace MeshByFaceNodes
+{
+    public class TwitterConnection
+    {
+        /// <summary>
+        /// This node takes the output of the MeshByFace.WebcamSelfie node (a filepath to a .jpg image as a string) and uses the EOS ML framework to derive a Mesh stored in the .obj format.
+        /// </summary>
+        /// <param name="filepathString">String: the fully-qualified filepath to the .jpeg created by the MeshByFace.WebcamSelfie node.</param>
+        /// <returns>String: the fully-qualified filepath of the generated .obj file.</returns>
+        /// <search>mesh, obj, jpeg, jpg, face</search>
+
+        public static void TweetSelfie(string filepathString)
+        {
+            char[] splitter = { '\r' };
+
+            string pythonExePath = @"python.exe";
+            string pyFilePath = @"C:\Users\Oliver\AppData\Local\FaceMesh\twitter.py";
+
+            Process process = new Process();
+            process.StartInfo.FileName = pythonExePath;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+
+            process.StartInfo.Arguments = string.Concat(pyFilePath, " ", @"C:\Users\Oliver\AppData\Local\FaceMesh\Images\bimselfie.jpg");
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
